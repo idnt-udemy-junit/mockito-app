4d462fbd2560664703b5430dbb645dd946aec681
indenaiten <angel.herce.soto@gmail.com>
2021-09-28 18:22:21 +0200

    feature #03 - Mockito // #040 - Realizando más pruebas del repositorio con el método guardar -Method "save" has been added in class "ExamRepository". -Method "saveSeveral" has been added in class "QuestionRepository". -Method "save" has been added in class "ExamService".

--- src/main/java/org/idnt/udemy/mockitoapp/example/service/ExamService.java
+++ src/main/java/org/idnt/udemy/mockitoapp/example/service/ExamService.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 public interface ExamService {
     Optional<Exam> findExamByName(String name);
     Optional<Exam> findExamByNameWithQuestions(String name);
+    Exam save(Exam exam);
 }
